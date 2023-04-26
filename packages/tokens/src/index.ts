8d227c4d5f509fdb35607c664c2b79008557d8a7
Eduardo Piorini <eduardo.piorini@stevanatogroup.com>
2023-04-26 11:10:19 +0200

    first commit

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,8 @@
+export * from './colors'
+
+export * from './space'
+export * from './font-weights'
+export * from './font-sizes'
+export * from './line-heights'
+export * from './fonts'
+export * from './radii'
